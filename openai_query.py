e17d7997374a79d1440e47eeba1cc51b0901e3d7
aoitech <aoi19900722@yahoo.co.jp>
2024-09-08 10:26:32 +0900

    音声を合成する処理を追加
    ・作成したクエリに沿って音声を合成する処理を追記

--- openai_query.py
+++ openai_query.py
@@ -4,10 +4,20 @@ import json
 url  = 'http://127.0.0.1:50021/'
 text = 'こんにちは'
 speaker_id = 3
-item_data={
+item_data = {
   'text': text,
   'speaker': speaker_id,
 }
-res = requests.post(url+'audio_query',params=item_data)
+res = requests.post(url+'audio_query',params = item_data)
 res_json = res.json()
 print(res_json)
+
+query_data = res_json
+a_params = {
+  'speaker': speaker_id
+}
+res = requests.post(
+  url+'synthesis', params = a_params,
+  data = json.dumps(query_data)
+  )
+print(res.content)
