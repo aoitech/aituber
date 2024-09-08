7cab945ea2e7d401ce10632194fef5ab2ced37d0
aoitech <aoi19900722@yahoo.co.jp>
2024-09-08 10:02:51 +0900

    クエリの作成
    ・VOICEVOXからクエリを取得する処理を記述

---
+++ openai_query.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+url  = 'http://127.0.0.1:50021/'
+text = 'こんにちは'
+speaker_id = 3
+item_data={
+  'text': text,
+  'speaker': speaker_id,
+}
+res = requests.post(url+'audio_query',params=item_data)
+res_json = res.json()
+print(res_json)
