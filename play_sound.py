94205907f2ec6709ed760fc5d35510267bf793f9
aoitech <aoi19900722@yahoo.co.jp>
2024-09-24 21:52:24 +0900

    音声データを再生する処理を追加
    ・VOICEVOXにアクセスし、クエリデータを作成
    ・仮想ファイルから音声データを読み取る
    ・テスト用の音声データ出力ファイルを作成

--- play_sound.py
+++ play_sound.py
@@ -16,30 +16,31 @@ class PlaySound:
       output_device_id
     ]
 
-    def _search_output_device_id(self, output_device_name, output_device_host_api=0) -> int:
-      #利用可能なデバイスの情報を取得
-      devices = sd.query_devices()
-      output_device_id = None
+  def _search_output_device_id(self, output_device_name, output_device_host_api=0) -> int:
+    #利用可能なデバイスの情報を取得
+    devices = sd.query_devices()
+    output_device_id = None
 
-      #指定された出力デバイス名とホストAPIに合致するデバイスIDを検索
-      for device in devices:
-        is_output_device_name = output_device_name in device["name"]
-        is_output_device_host_api = device["hostapi"] == output_device_host_api
-        if is_output_device_name and is_output_device_host_api:
-          output_device_id = device["index"]
-          break
+    #指定された出力デバイス名とホストAPIに合致するデバイスIDを検索
+    for device in devices:
+      is_output_device_name = output_device_name in device["name"]
+      is_output_device_host_api = device["hostapi"] == output_device_host_api
+      if is_output_device_name and is_output_device_host_api:
+        output_device_id = device["index"]
+        break
 
-      #合致するデバイスが見つからなかった場合の処理
-      if output_device_id is None:
-        print("output_deviceが見つかりませんでした")
-        exit()
+    #合致するデバイスが見つからなかった場合の処理
+    if output_device_id is None:
+      print("output_deviceが見つかりませんでした")
+      exit()
 
-        return output_device_id
-      def play_sound(self, data, rate) -> bool:
-        #音声データを再生
-        sd.play(data, rate)
+      return output_device_id
 
-        #再生が完了するまで待機
-        sd.wait()
+  def play_sound(self, data, rate) -> bool:
+    #音声データを再生
+    sd.play(data, rate)
 
-        return True
+    #再生が完了するまで待機
+    sd.wait()
+
+    return True
